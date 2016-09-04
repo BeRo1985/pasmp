720c7cb33e055ef92f44192a1403108167e322fc
Benjamin Rosseaux <benjamin@rosseaux.de>
2016-09-04 15:58:25 +0200

    - Added missing header into PasMPProfilerHistoryView.pas

--- src/PasMPProfilerHistoryView.pas
+++ src/PasMPProfilerHistoryView.pas
@@ -1,3 +1,55 @@
+(******************************************************************************
+ *                                   PasMP                                    *
+ ******************************************************************************
+ *                        Version 2016-09-04-15-50-0000                       *
+ ******************************************************************************
+ *                                zlib license                                *
+ *============================================================================*
+ *                                                                            *
+ * Copyright (C) 2016, Benjamin Rosseaux (benjamin@rosseaux.de)               *
+ *                                                                            *
+ * This software is provided 'as-is', without any express or implied          *
+ * warranty. In no event will the authors be held liable for any damages      *
+ * arising from the use of this software.                                     *
+ *                                                                            *
+ * Permission is granted to anyone to use this software for any purpose,      *
+ * including commercial applications, and to alter it and redistribute it     *
+ * freely, subject to the following restrictions:                             *
+ *                                                                            *
+ * 1. The origin of this software must not be misrepresented; you must not    *
+ *    claim that you wrote the original software. If you use this software    *
+ *    in a product, an acknowledgement in the product documentation would be  *
+ *    appreciated but is not required.                                        *
+ * 2. Altered source versions must be plainly marked as such, and must not be *
+ *    misrepresented as being the original software.                          *
+ * 3. This notice may not be removed or altered from any source distribution. *
+ *                                                                            *
+ ******************************************************************************
+ *                  General guidelines for code contributors                  *
+ *============================================================================*
+ *                                                                            *
+ * 1. Make sure you are legally allowed to make a contribution under the zlib *
+ *    license.                                                                *
+ * 2. The zlib license header goes at the top of each source file, with       *
+ *    appropriate copyright notice.                                           *
+ * 3. After a pull request, check the status of your pull request on          *
+      http://github.com/BeRo1985/pasmp                                        *
+ * 4. Write code, which is compatible with Delphi 7-XE7 and FreePascal >= 2.6 *
+ *    so don't use generics/templates, operator overloading and another newer *
+ *    syntax features than Delphi 7 has support for that, but if needed, make *
+ *    it out-ifdef-able.                                                      *
+ * 5. Don't use Delphi-only, FreePascal-only or Lazarus-only libraries/units, *
+ *    but if needed, make it out-ifdef-able.                                  *
+ * 6. No use of third-party libraries/units as possible, but if needed, make  *
+ *    it out-ifdef-able.                                                      *
+ * 7. Try to use const when possible.                                         *
+ * 8. Make sure to comment out writeln, used while debugging.                 *
+ * 9. Make sure the code compiles on 32-bit and 64-bit platforms (x86-32,     *
+ *    x86-64, ARM, ARM64, etc.).                                              *
+ * 10. Make sure the code runs on platforms with weak and strong memory       *
+ *     models without any issues.                                             *
+ *                                                                            *
+ ******************************************************************************)
 unit PasMPProfilerHistoryView;
 {$ifdef fpc}
  {$mode delphi}
